c88b0859b159beeca3c49fc45c0e2afa927fd07a
Philipp Schirmacher <philipp.schirmacher@innoq.com>
2014-05-27 14:23:51 +0200

    collection utility methods

--- app/models/Collection.scala
+++ app/models/Collection.scala
@@ -1,3 +1,22 @@
 package models
 
-case class Collection[T](items: List[T], offset: Int, total: Long)
+case class Collection[T](items: List[T], offset: Int, total: Long) {
+
+  def hasNext = (items.size + offset) < total
+
+  def hasPrevious = offset > 0
+
+  val maxCount = 10
+
+  def nextOffset = offset + maxCount
+
+  def displaying = {
+    if (!items.isEmpty) {
+      val from = offset + 1
+      val to = from + items.size
+      s"Displaying $from to $to of $total."
+    } else {
+      "Nothing to display."
+    }
+  }
+}
