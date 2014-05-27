945f3e1631612093a65e9b082015eca2ca2f02d6
Jan Nikolai Trzeszkowski <jan.nikolai.trzeszkowski@innoq.com>
2014-05-27 15:08:27 +0200

    Add pagination

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,5 +1,6 @@
 package controllers
 
+import _root_.java.lang.Math
 import play.api.mvc.{Action, Controller}
 import models.Issue
 import java.util.Date
@@ -13,7 +14,21 @@ object Application extends Controller {
     Issue.save(Issue(System.currentTimeMillis(), "projectName", "priority", "issueType", "summary", new Exception().toString,
       "description", "reporter", "componentName", "componentVersion", "processingState", new Date(), new Date(), "closeAction",
       "hannes", "comment"))
+
     val issues = Issue.load(offset, count)
-    Ok(views.html.issues("List of " + issues.items.size + " issue(s) (total: " + issues.total + ")", issues.items))
+    var links = Map[String, String]()
+
+    if(offset > 0)
+      links += ("prev" -> routes.Application.issues(Math.max(0, offset-count), count).toString)
+
+    val nextOffset = offset+count
+    if(nextOffset < issues.total)
+      links += ("next" -> routes.Application.issues(nextOffset, count).toString)
+
+    Ok(views.html.issues(
+      "List of " + issues.items.size + " issue(s) (total: " + issues.total + ")",
+      issues.items,
+      links
+    ))
   }
 }
