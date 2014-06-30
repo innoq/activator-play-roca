d690f4f9adca2ebae660fd44a7f0c6d485f56a38
FND <fnd@localhost.localdomain>
2014-06-30 16:09:40 +0200

    added client-side validation
    NB:
    * this is very much simplistic and only meant to illustrate the basic
      approach
    * the server-side adjustments (populating the error element and removing
      the "hidden" class) have yet to be implemented
    * there is no UI notification if a validation error occurs outside the
      viewport

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,9 +11,33 @@ require.config({
 });
 
 require(["jquery", "pjax"], function($) {
+	$("form[novalidate]").on("submit", validateForm);
+
 	var selector = "#contents";
 	$(document.body).pjax("a", selector);
 	$(document).on("submit", "form[role=search]", function(ev) {
 		$.pjax.submit(ev, selector)
-	})
+	});
+
+	function validateForm(ev) { // XXX: simplistic, for illustration purposes only
+		var fields = $("input, textarea, select", this).filter("[data-on-error]");
+		var errors = fields.map(function(i, node) {
+			var field = $(node);
+			var group = field.closest(".form-group"); // XXX: Bootstrap-specific
+			var error = group.find(".error");
+			var message = field.data("on-error"); // XXX: misleading attribute name
+
+			if(node.validity.valid ||
+					!message || !error.length) { // can't cope client-side
+				error.text("").addClass("hidden");
+				return null;
+			}
+
+			error.text(message).removeClass("hidden");
+			return node;
+		});
+		if(errors.length) {
+			ev.preventDefault();
+		}
+	}
 });
