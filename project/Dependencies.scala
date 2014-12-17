fad4e1aff5f0a6584f8bc328c4c549f972a4d127
Daniel Westheide <daniel.westheide@innoq.com>
2014-12-17 10:43:23 +0100

    Update to Play 2.3.7, Scala 2.11.4 and SBT 0.13.7

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,15 +6,14 @@ object Dependencies {
     val webJars = "org.webjars"
   }
 
-  val jodaTime = "joda-time" % "joda-time" % "2.3"
-  val jodaConvert = "org.joda" % "joda-convert" % "1.6"
+  val jodaTime = "joda-time" % "joda-time" % "2.6"
+  val jodaConvert = "org.joda" % "joda-convert" % "1.7"
 
   val halselhof = uri("git://github.com/tobnee/HALselhof.git")
 
-  val webJarsPlay = Organizations.webJars %% "webjars-play" % "2.3.0"
-  val bootstrap = Organizations.webJars % "bootstrap" % "3.1.1-1"
+  val webJarsPlay = Organizations.webJars %% "webjars-play" % "2.3.0-2"
+  val bootstrap = Organizations.webJars % "bootstrap" % "3.3.1"
 
-  val macwire = "com.softwaremill.macwire" %% "macros" % "0.7"
-
+  val macwire = "com.softwaremill.macwire" %% "macros" % "0.7.3"
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,12 +1,11 @@
-// Comment to get more information during initialization
 logLevel := Level.Warn
 
 // The Typesafe repository
 resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 
 // Use the Play sbt plugin for Play projects
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.3")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.7")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.1.0")
+addSbtPlugin("com.typesafe.sbt" % "sbt-web" % "1.1.1")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-rjs" % "1.0.1")
+addSbtPlugin("com.typesafe.sbt" % "sbt-rjs" % "1.0.7")
